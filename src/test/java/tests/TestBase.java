29b2ec99783dc2bd1d3287b07a85204f3b3cd02c
iStarzG <driftcd@mail.ru>
2024-03-07 02:14:41 +0500

    changed TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.DriverConfig;
 import helpers.Attach;
@@ -42,6 +43,7 @@ public class TestBase {
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
+        Selenide.closeWebDriver();
     }
 }
 
